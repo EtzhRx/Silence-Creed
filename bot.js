6752b1a987ecfd9c9147f61b843f2de4408b2099
EtzhRx <30803966+EtzhRx@users.noreply.github.com>
2018-09-21 00:51:34 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const YouTube = require('simple-youtube-api');
+const { TOKEN, PREFIX, GOOGLE_API_KEY } = require('./config');
 const ytdl = require('ytdl-core');
 
 const client = new Client({ disableEveryone: true });
