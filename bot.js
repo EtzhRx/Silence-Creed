6e177bb4b2c944927541c75f3859c15a7eb8762d
EtzhRx <30803966+EtzhRx@users.noreply.github.com>
2018-09-23 14:48:31 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
- 
+const bot = new Discord.Client( 
 // Set the prefix
 let prefix = "!sc";
 client.on("message", (message) => {
@@ -15,31 +15,12 @@ client.on("message", (message) => {
     message.channel.send("gak bisa play music aowkowkowkook");
   }
    
-  if (message.content.startsWith(prefix + "play")) {
-    message.channel.send("gak bisa play music aowkowkowkook");
+  if (message.content.startsWith(prefix + "image")) {
+   message.channel.send("My Bot's message", {files: ["https://i.imgur.com/XxxXxXX.jpg"]});
   }
+ 
 });
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
 
-
-//random image
-var Discord = require('discord.js');
-var bot = new Discord.Client();
-
-bot.on('message', message => {
-    var prefix = '!'
-    var msg = message.content;
-
-    if (msg === prefix + 'image') {
-        message.channel.send('Message that goes above image', {
-            files: [
-                "./image-to-send.png"
-            ]
-        });
-    }
-});
-
-bot.login('TOKEN');
-shareimprove this answer
