60e152f4a9873d5cc94c0af38b2c4968cb8f5acd
EtzhRx <30803966+EtzhRx@users.noreply.github.com>
2018-09-23 14:13:22 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,8 +7,8 @@ client.on("message", (message) => {
   // Exit and stop if the prefix is not there or if user is a bot
   if (!message.content.startsWith(prefix) || message.author.bot) return;
  
-  if (message.content.startsWith(prefix + "ping")) {
-    message.channel.send("pong!");
+  if (message.content.startsWith(prefix + "sc")) {
+    message.channel.send("Test!");
   } else
   if (message.content.startsWith(prefix + "foo")) {
     message.channel.send("bar!");
