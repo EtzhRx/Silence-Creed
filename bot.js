44b4010b4e01fcf2d4a3714cb2f9da1edc10ac67
EtzhRx <30803966+EtzhRx@users.noreply.github.com>
2018-09-23 14:53:29 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 
 // Set the prefix
-let prefix = "!sc";
+let prefix = "sc ";
 client.on("message", (message) => {
   // Exit and stop if the prefix is not there or if user is a bot
   if (!message.content.startsWith(prefix) || message.author.bot) return;
