571aae26498110b2b16ec140101cb88c1d1152ab
EtzhRx <30803966+EtzhRx@users.noreply.github.com>
2018-09-27 23:21:40 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,18 +5,19 @@ const client = new Discord.Client();
 let prefix = "!";
 client.on("message", (message) => {
   // Exit and stop if the prefix is not there or if user is a bot
-
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+ 
+  if (message.content.startsWith(prefix + "skip")) {
+    message.channel.send("Oni-Chan !");
+  } else
+   
   if (message.content.startsWith(prefix + "p")) {
-    message.channel.send(".......");
+    message.channel.send("Oni-Chan !");
   }
    
   if (message.content.startsWith(prefix + "play")) {
-    message.channel.send(".......");
+    message.channel.send("Oni-Chan !");
   }
- 
-  if (message.content.startsWith(prefix + "s")) {
-    message.channel.send(".......");
-  } 
 });
 
 // THIS  MUST  BE  THIS  WAY
