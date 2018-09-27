77b9228008dc43bd58da3da80c7d148daefa8300
EtzhRx <30803966+EtzhRx@users.noreply.github.com>
2018-09-27 22:53:06 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,22 +7,17 @@ client.on("message", (message) => {
   // Exit and stop if the prefix is not there or if user is a bot
   if (!message.content.startsWith(prefix) || message.author.bot) return;
  
-   (message.content.startsWith(prefix + "play")) {
-    message.channel.send("Playing.....");
+  if (message.content.startsWith(prefix + "play")) {
+    message.channel.send("Playing......");
   } else
    
-   (message.content.startsWith(prefix + "p")) {
-    message.channel.send("Playing.....");
+  if (message.content.startsWith(prefix + "p")) {
+    message.channel.send("Playing......");
   }
    
- (message.content.startsWith(prefix + "skip")) {
-    message.channel.send("Skip.....");
+  if (message.content.startsWith(prefix + "s")) {
+    message.channel.send("Skip......");
   }
- 
-  (message.content.startsWith(prefix + "s")) {
-    message.channel.send("Skip.....");
-  }
-   
 });
 
 // THIS  MUST  BE  THIS  WAY
